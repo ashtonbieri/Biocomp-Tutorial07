26f680fc652d000c13973ead136dd298ad682881
Ashton Bieri <abieri@crcfe01.crc.nd.edu>
2021-10-08 11:01:41 -0400

    Minor edits

--- tutorial7.sh
+++ tutorial7.sh
@@ -1,7 +1,7 @@
 #Solutions for Tutorial 7, Intro to Biocomputing
 #Ashton Bieri / netid abieri / github:ashtonbieri
 #Challenge 1
-~/hammer -in sigma70.ref -out sigma70aligned.ref
+~/muscle -in sigma70.refs -out sigma70aligned.refs
 
 #Challenge 2
 
