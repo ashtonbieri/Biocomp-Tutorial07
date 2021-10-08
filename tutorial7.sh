0c5ea30e943250c8483e14ebcd839dad111c9269
Ashton Bieri <abieri@crcfe01.crc.nd.edu>
2021-10-08 10:50:26 -0400

    Added tutorial 7.sh, added solution to Challenge 1

---
+++ tutorial7.sh
@@ -0,0 +1,10 @@
+#Solutions for Tutorial 7, Intro to Biocomputing
+#Ashton Bieri / netid abieri / github:ashtonbieri
+#Challenge 1
+~/hammer -in sigma70.ref -out sigma70aligned.ref
+
+#Challenge 2
+
+
+
+#Challenge 3
